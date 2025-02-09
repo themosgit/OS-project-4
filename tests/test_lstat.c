6b7c5ca2f9c9e49ca9e18d59b43e0373a6ed4996
themosgit <themos360@gmail.com>
2025-02-09 19:33:53 +0200

    lstat test

---
+++ tests/test_lstat.c
@@ -0,0 +1,60 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  const char *filename = argv[1];
+  struct stat file_stat;
+  if (lstat(filename, &file_stat) == -1) {
+    perror("lstat");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("filename = %s\n", filename);
+  printf("Type : ");
+  if (S_ISREG(file_stat.st_mode)) {
+    printf("regular file\n");
+  } else if (S_ISDIR(file_stat.st_mode)) {
+    printf("directory\n");
+  } else if (S_ISLNK(file_stat.st_mode)) {
+    printf("sym-link\n");
+  } else if (S_ISFIFO(file_stat.st_mode)) {
+    printf("fifo pipe\n");
+  } else if (S_ISSOCK(file_stat.st_mode)) {
+    printf("socket\n");
+  } else if (S_ISCHR(file_stat.st_mode)) {
+    printf("character device\n");
+  } else if (S_ISBLK(file_stat.st_mode)) {
+    printf("block device\n");
+  } else {
+    printf("unknown\n");
+  }
+
+  printf("Size: %ld\n", file_stat.st_size);
+  printf("Inode: %ld\n", file_stat.st_ino);
+  printf("Links: %ld\n", file_stat.st_nlink);
+  printf("Permissions: %o\n", file_stat.st_mode & 0777);
+  printf("Owner id: %d\n", file_stat.st_uid);
+  printf("Group id: %r\n", file_stat.st_gid);
+
+  char time_str[100];
+
+  strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", localtime(&file_stat.st_atime));
+  printf("Access time: %s\n", time_str);
+
+  strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", localtime(&file_stat.st_mtime));
+  printf("Modify time: %s\n", time_str);
+
+  strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", localtime(&file_stat.st_ctime));
+  printf("Change time: %s\n", time_str);
+
+  return EXIT_SUCCESS;
+}
